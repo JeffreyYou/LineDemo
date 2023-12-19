9bfbf64803cfee2401ac9f330a34544c8174a809
11weioswald <50092436+11weioswald@users.noreply.github.com>
2023-12-19 00:07:08 -0500

    Add files via upload

--- WebHook Server/src/main/java/com/jeffrey/linedemo/service/OpenaiService.java
+++ WebHook Server/src/main/java/com/jeffrey/linedemo/service/OpenaiService.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.jeffrey.linedemo.entity.GreenMessage;
 import com.jeffrey.linedemo.entity.OpenAIMessage;
 import com.jeffrey.linedemo.utils.GreenApiUtils;
+import com.jeffrey.linedemo.utils.MessageHandleUtils;
 import com.jeffrey.linedemo.utils.WebSocketUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,31 +24,32 @@ public class OpenaiService {
     GreenApiUtils greenApiUtils;
     @Autowired
     Gson gson;
+
+    @Autowired
+    MessageHandleUtils messageHandleUtils;
+
     public void sendMessage(GreenMessage data) {
         String phone = data.getSenderData().getSender();
         String message = data.getMessageData().getTextMessageData().getTextMessage();
         String chatId = data.getSenderData().getChatId();
         handleRequest(phone, message, chatId);
+        /*messageHandleUtils.setfirstReceivedMessage(data);*/
     }
 
 
     public void handleRequest(String phone, String message, String chatId) {
         try {
             System.out.println("[User]: " + message);
+            System.out.print("[AI]: ");
             WebSocketSession session = webSocketUtils.createSessionIfNotExist(phone, chatId);
 
-            if (message.startsWith("[Day")) {
-                if (message.equals("[Day1]")) {
-                    greenApiUtils.sendMessageToUser("謝謝你的聯繫,你好我是三浦老師的助理,村崎幸子,請多多關照。", chatId);
-                    greenApiUtils.sendMessageToUser("能請教一下怎麼稱呼您嗎？", chatId);
-                }
-            } else {
+            if (messageHandleUtils.isNotification(message)){
+                messageHandleUtils.SendNotification(phone,message,chatId);
+            }else{
                 String jsonMessage = gson.toJson(new OpenAIMessage(message));
                 TextMessage aiMessage = new TextMessage(jsonMessage);
                 session.sendMessage(aiMessage);
             }
-
-
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
